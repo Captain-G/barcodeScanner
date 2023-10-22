0a033645214b18503ec323c2794b0e25af9a336e
Captian-G <poppinpites7@gmail.com>
2023-10-22 12:10:34 +0300

    Initial commit

--- main.py
+++ main.py
@@ -14,33 +14,24 @@ options = Options()
 options.add_experimental_option("detach", True)
 
 
-# Make one method to decode the barcode
 def BarcodeReader(image):
-    # read the image in numpy array using cv2
     img = cv2.imread(image)
 
-    # Decode the barcode image
     detectedBarcodes = decode(img)
 
-    # If not detected then print the message
     if not detectedBarcodes:
         print("Barcode Not Detected or your barcode is blank/corrupted!")
     else:
 
-        # Traverse through all the detected barcodes in image
         for barcode in detectedBarcodes:
 
-            # Locate the barcode position in image
             (x, y, w, h) = barcode.rect
 
-            # Put the rectangle in image using
-            # cv2 to highlight the barcode
             cv2.rectangle(img, (x - 10, y - 10),
                           (x + w + 10, y + h + 10),
                           (255, 0, 0), 2)
 
             if barcode.data != "":
-                # Print the barcode data
                 split_code = str(barcode.data).split("'")[1]
                 print(f"Barcode.data : {split_code}")
                 # print(f"Barcode.type : {barcode.type}")
@@ -81,6 +72,5 @@ def BarcodeReader(image):
 
 
 if __name__ == "__main__":
-    # Take the image from user
     image = "WhatsApp Image 2023-10-22 at 11.56.09.jpeg"
     BarcodeReader(image)
